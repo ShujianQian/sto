62f855b282e1e28833952ae671400c786507552b
Shujian Qian <shujian.qian@outlook.com>
2023-04-12 07:31:17 -0400

    add time in script.

--- run_my_ycsb.sh
+++ run_my_ycsb.sh
@@ -1,11 +1,11 @@
-# ALPHAS=("0.0" "0.2" "0.4" "0.6" "0.8" "0.9" "0.95" "0.99")
-ALPHAS=("0.0")
+ALPHAS=("0.0" "0.2" "0.4" "0.6" "0.8" "0.9" "0.95" "0.99")
 READ_TYPES=("full" "field")
 CC_TYPES=("default" "tictoc" "mvcc")
 YCSB_BENCH=("A" "B" "C" "F")
 OUTPUT_DIR=$1
 
 mkdir -p "$OUTPUT_DIR"
+START_TIME=$SECOND
 
 for cc_type in "${CC_TYPES[@]}"; do
     for read_type in "${READ_TYPES[@]}"; do
@@ -16,6 +16,8 @@ for cc_type in "${CC_TYPES[@]}"; do
                     filename="${OUTPUT_DIR}/${cc_type}_${read_type}_${alpha}_YCSB${ycsb_bench}_${repeat}.txt"
                     ./ycsb_bench -t32 "-m${ycsb_bench}" "-i${cc_type}" -g -x "-s${alpha}" "-r${read_type}" > "$filename"
                 done
+                ELAPSED_TIME=$((SECONDS - START_TIME))
+                echo "Current Runtime ${ELAPSED_TIME} s"
             done
         done
     done
